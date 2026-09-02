1254dc559996e2eb3bdc8d72ca1b70e9e6bcaf70
JaeWonChoi <78068768+choi06151@users.noreply.github.com>
2026-09-02 11:20:00 +0900

    feat : y 높이 제한 개발

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -3,20 +3,30 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     //목적 : 키보드 입력에 따라서 플레이어 이동 처리를 하고싶다 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-  
-    // Update is called once per frame
 
-    [SerializeField]private float _moveSpeed = 3f;
-    void Update()
+
+
+     public float MoveSpeed = 3f;
+     public float[] constraintYRange = new float[2]{-5f,-1.5f}; //-1.5 , -5 //이동 가능범위
+
+     void Update()
     {
-        float h=Input.GetAxis("Horizontal"); //키보드 왼/ 오른쪽 입력상태에 따라 -1f ~ 1f
-        float v = Input.GetAxis("Vertical"); //키보드 위 /아래 -1f~1f
-        Debug.Log($"h:{h} v:{v}");
+        float h=Input.GetAxisRaw("Horizontal"); //키보드 왼/ 오른쪽 입력상태에 따라 -1f ~ 1f
+        float v = Input.GetAxisRaw("Vertical"); //키보드 위 /아래 -1f~1f
+        //Raw를 추가하면 중간값 없어짐 
+        //Debug.Log($"h:{h} v:{v}");
         
-        Vector2 direction = new Vector2(h,v); // 왼쪽방향
-                                              //현재 방향과 속도에 따라 이동한다
-        transform.Translate(direction * Time.deltaTime*_moveSpeed);
+        Vector2 normalDirection = new Vector2(h,v);         //현재 방향과 속도에 따라 이동한다
+        Vector2 normalizedDirection = normalDirection.normalized; //정규화
+        Vector3 nextPosition=transform.position+(Vector3)normalizedDirection * Time.deltaTime * MoveSpeed;
+
+        if (nextPosition.y >= constraintYRange[0] && nextPosition.y <= constraintYRange[1]) //범위 내부여야만 이동
+        {
+            transform.Translate(normalizedDirection * Time.deltaTime * MoveSpeed);
+
+        }
+
+        
           
           
       
