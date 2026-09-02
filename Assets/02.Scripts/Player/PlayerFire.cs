a0166d957e89c48cf1163804a94321d19cb4701e
JaeWonChoi <78068768+choi06151@users.noreply.github.com>
2026-09-02 15:19:05 +0900

    feat: 총알 쿨타임 및 자동 발사 기능 개발

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -1,15 +1,20 @@
+using System.Threading;
 using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
     public GameObject MainBulletPrefab;
-    public static int MainBulletCount=2;
+    public const int MainBulletCount=2;
     public Transform[] MainFirePoint=new Transform[MainBulletCount];
     
     public GameObject SubBulletPrefab;
-    public static int SubBulletCount=2;
+    public const int SubBulletCount=2;
     public Transform[] SubFirePoint=new Transform[MainBulletCount];
 
+    public float FireCoolTime=0.1f;
+    
+    private bool _isAutoFire=false;
+    private float _currentFireCooldown=999f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,27 +26,82 @@ public class PlayerFire : MonoBehaviour
     void Update()
     {
         CheckFire();
+        AutoFire();
     }
 
     private void CheckFire()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            for (int i = 0; i < MainBulletCount; i++)
-            {
-                CreateCommand createCommand=new CreateCommand(this.gameObject,MainBulletPrefab,MainFirePoint[i].position);
-                CommandManager.Instance.ExecuteCommand(createCommand);
-            }
-            
-            for (int i = 0; i < SubBulletCount; i++)
-            {
-                CreateCommand createCommand=new CreateCommand(this.gameObject,SubBulletPrefab,SubFirePoint[i].position);
-                CommandManager.Instance.ExecuteCommand(createCommand);
 
+        UpdateFireCoolTime();
+        
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _isAutoFire = !_isAutoFire;
+            if (_isAutoFire)
+            {
+                Debug.Log("Auto Fire활성화");
             }
-            
+            else
+            {
+                Debug.Log("Auto Fire비활성화");
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space)&&CheckFireCoolTime())
+        {
+            FireAllBullet();
+        }
+        
+    }
+
+    private void FireMainBullet()
+    {
+        for (int i = 0; i < MainBulletCount; i++)
+        {
+            CreateCommand createCommand=new CreateCommand(this.gameObject,MainBulletPrefab,MainFirePoint[i].position);
+            CommandManager.Instance.ExecuteCommand(createCommand);
         }
     }
     
+
+    private void FireSubBullet()
+    {
+        for (int i = 0; i < SubBulletCount; i++)
+        {
+            CreateCommand createCommand=new CreateCommand(this.gameObject,SubBulletPrefab,SubFirePoint[i].position);
+            CommandManager.Instance.ExecuteCommand(createCommand);
+        }
+    }
+
+    private void FireAllBullet()
+    {
+        _currentFireCooldown = 0;
+
+        FireMainBullet();
+        FireSubBullet();
+    }
+    private void AutoFire()
+    {
+        if (_isAutoFire)
+        {
+            if (_currentFireCooldown > FireCoolTime)
+            {
+
+                FireAllBullet();
+            }
+        }
+    }
+
+    private bool CheckFireCoolTime()
+    {
+        return _currentFireCooldown > FireCoolTime;
+    }
+
+    private void UpdateFireCoolTime()
+    {
+        _currentFireCooldown+=Time.deltaTime;
+        Debug.Log(_currentFireCooldown);
+    }
     
 }
