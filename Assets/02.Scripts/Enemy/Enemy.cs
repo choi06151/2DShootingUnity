81c6c30cb01f60318012f8c395b9ad6749e47577
JaeWonChoi <78068768+choi06151@users.noreply.github.com>
2026-09-03 12:48:11 +0900

    feat: 충돌 기능 개발

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -1,9 +1,19 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public float MoveSpeed = 3f;
     private Vector3 _direction = new Vector3(0, -1, 0);
+
+    private float _hp = 100;
+
+    public float Hp
+    {
+        get => _hp;
+        set => _hp = value;
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,5 +31,31 @@ public class Enemy : MonoBehaviour
         MovementCommand movementCommand = new MovementCommand(this.gameObject,
             _direction * Time.deltaTime * MoveSpeed);
         CommandManager.Instance.ExecuteCommand(movementCommand);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+        }
+        else if (other.tag == "PlayerBullet")
+        {
+            Destroy(other.gameObject);
+            GetDamage(100);
+        }
+    }
+
+    private void GetDamage(float damage)
+    {
+        _hp -= damage;
+        if (_hp <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
     }
 }
