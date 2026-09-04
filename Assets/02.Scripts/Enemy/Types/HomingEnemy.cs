ecd5f07a026bf172b656dfbda080f75b713a34ad
JaeWonChoi <78068768+choi06151@users.noreply.github.com>
2026-09-04 10:26:22 +0900

    test: 테스트를 위한 변경

--- Assets/02.Scripts/Enemy/Types/HomingEnemy.cs
+++ Assets/02.Scripts/Enemy/Types/HomingEnemy.cs
@@ -9,11 +9,12 @@ public class HomingEnemy : Enemy
 
     private void Start()
     {
-        _playerMove = CommandManager.Instance.PlayerMove;
     }
 
     protected override void Move()
     {
+        _playerMove = CommandManager.Instance.PlayerMove;
+
         _direction = (_playerMove.transform.position - transform.position).normalized;
         MovementCommand movementCommand = new MovementCommand(this.gameObject,
             _direction * Time.deltaTime * MoveSpeed);
